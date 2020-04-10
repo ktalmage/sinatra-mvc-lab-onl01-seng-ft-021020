d26196cfa2be8fe0fe8cffdb7422be99421203a2
Kyle Talmage <ktalmage85@gmail.com>
2020-04-10 14:47:32 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,6 +14,7 @@ def piglatinize(user_phrase)
   
   def to_pig_latin(sentence)
     words_in_sentence = sentence.split(" ")
+    binding.pry
     pig_latinized_words = words_in_sentence.each do |word|
     self.piglatinize(word)
   end
